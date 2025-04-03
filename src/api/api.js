f9abe1fffc0b24c82e80731a54da002ce79b09f4
alfredoT7 <123456alfredotorri@gmail.com>
2025-04-03 15:15:43 -0400

    vecel config

---
+++ src/api/api.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+import { data } from 'framer-motion/client';
+const baseURL = "http://localhost:3000";
+
+const api = axios.create({
+  baseURL: baseURL,
+  responseType: 'json',
+  withCredentials: true, 
+  timeout: 100000,
+});
+api.interceptors.request.use((config) => {
+  if (config.data instanceof FormData) {
+    config.headers['Content-Type'] = 'multipart/form-data';
+  } else {
+    config.headers['Content-Type'] = 'application/json';
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+export default api;
+
+export const sendMessage = (data) => api.post('/api/email/messages', data)
