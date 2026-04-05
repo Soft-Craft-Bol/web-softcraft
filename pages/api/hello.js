0dfd4511e6b67524d7deb6114848928e19a70f67
gaspar200309 <133708167+gaspar200309@users.noreply.github.com>
2026-04-05 18:08:09 -0400

    fix: update api hello

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+export const config = {
+  runtime: 'edge',
+};
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler() {
+  return new Response(
+    JSON.stringify({ ok: true }),
+    {
+      headers: { 'content-type': 'application/json' },
+    }
+  );
 }
